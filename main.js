3b16a8e44591e20316cb0b0956683bc9db1048cf
roseaika05@gmail.com <roseaika05.gmail.com>
2022-11-07 12:34:08 +0600

    navbar

---
+++ main.js
@@ -0,0 +1,19 @@
+const API = "http://localhost:8000/infor";
+
+let closeModalBtn = document.querySelector("#close-modal");
+let statusImg = document.querySelector("#statusImgUrl");
+let nikName = document.querySelector("#nikName");
+let adress = document.querySelector("#location");
+let mainPostImg = document.querySelector("#mainPostImg");
+let details = document.querySelector("#details");
+
+let sectionRead = document.querySelector("#section-read");
+
+let postCard = document.querySelector("#filter-post");
+let mainModal = document.getElementsByClassName("main-modal")[0];
+let closeModal = document.querySelector("#close-modal");
+
+let closeModalEdit = document.querySelector("#close-modal2");
+let statusImgEdit = document.querySelector("#statusImgUrlEdit");
+let nikNameEdit = document.querySelector("#nikNameEdit");
+let adressEdit = document.querySelector("locationEdit");
